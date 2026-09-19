18e5af73ba8a649c7356cee49733ff52a42cef84
Ali Madooei <madooei@jhu.edu>
2026-09-19 14:29:54 -0400

    Regenerate starter from updated master

--- src/test/practice/BagTest.java
+++ src/test/practice/BagTest.java
@@ -26,77 +26,37 @@ public class BagTest {
 
   @Test
   public void addIncreasesSize() {
-    bag.add(BallColor.BLACK);
-    assertEquals(1, bag.size());
-    bag.add(BallColor.BLUE);
-    assertEquals(2, bag.size());
+    // TODO: Implement me
+    throw new UnsupportedOperationException("TODO: Implement me");
   }
 
   @Test
   public void removeFromEmptyBagThrows() {
-    try {
-      bag.remove();
-      fail("expected IllegalStateException when removing from an empty bag");
-    } catch (IllegalStateException e) {
-      return;
-    }
+    // TODO: Implement me
+    throw new UnsupportedOperationException("TODO: Implement me");
   }
 
   @Test
   public void removeShrinksSizeByOne() {
-    bag.add(BallColor.BLACK);
-    bag.add(BallColor.BLUE);
-    bag.remove();
-    assertEquals(1, bag.size());
+    // TODO: Implement me
+    throw new UnsupportedOperationException("TODO: Implement me");
   }
 
   @Test
   public void removeReturnsABallThatWasAdded() {
-    bag.add(BallColor.BLACK);
-    bag.add(BallColor.BLACK);
-    assertEquals(BallColor.BLACK, bag.remove());
+    // TODO: Implement me
+    throw new UnsupportedOperationException("TODO: Implement me");
   }
 
   @Test
   public void drainingReturnsEveryBallAdded() {
-    bag.add(BallColor.BLACK);
-    bag.add(BallColor.BLACK);
-    bag.add(BallColor.BLACK);
-    bag.add(BallColor.BLUE);
-    bag.add(BallColor.BLUE);
-
-    int black = 0;
-    int blue = 0;
-    while (bag.size() > 0) {
-      BallColor drawn = bag.remove();
-      if (drawn == BallColor.BLACK) {
-        black++;
-      } else {
-        blue++;
-      }
-    }
-
-    assertEquals(3, black);
-    assertEquals(2, blue);
-    assertEquals(0, bag.size());
+    // TODO: Implement me
+    throw new UnsupportedOperationException("TODO: Implement me");
   }
 
   @Test
   public void addingPastInitialCapacityKeepsEveryBall() {
-    for (int i = 0; i < 25; i++) {
-      bag.add(BallColor.BLUE);
-    }
-
-    assertEquals(25, bag.size());
-
-    int blue = 0;
-    while (bag.size() > 0) {
-      if (bag.remove() == BallColor.BLUE) {
-        blue++;
-      }
-    }
-
-    assertEquals(25, blue);
-    assertEquals(0, bag.size());
+    // TODO: Implement me
+    throw new UnsupportedOperationException("TODO: Implement me");
   }
 }
